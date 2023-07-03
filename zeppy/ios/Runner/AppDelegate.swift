5cee3eae27bb8e659e7c603075e92509b773129b
hexaspace <hexa6do@naver.com>
2023-07-03 16:21:44 +0900

    [TAS-64] Feat:  background locator 2 예제실행 및 테스트

--- zeppy/ios/Runner/AppDelegate.swift
+++ zeppy/ios/Runner/AppDelegate.swift
@@ -1,7 +1,11 @@
 import UIKit
 import Flutter
+// for package google_maps_flutter
+import GoogleMaps
+// for package background_locator_2
 import background_locator_2
 
+    // for package background_locator_2
 func registerPlugins(registry: FlutterPluginRegistry) -> () {
     if (!registry.hasPlugin("BackgroundLocatorPlugin")) {
         GeneratedPluginRegistrant.register(with: registry)
@@ -13,7 +17,10 @@ func registerPlugins(registry: FlutterPluginRegistry) -> () {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // for package google_maps_flutter
+    GMSServices.provideAPIKey("YOUR KEY HERE")
     GeneratedPluginRegistrant.register(with: self)
+    // for package background_locator_2
     BackgroundLocatorPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
